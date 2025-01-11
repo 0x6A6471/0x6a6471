bf9f656d818f37b59cc2f394471c991ab7a64ada
0x6A6471 <hello@jakequinter.io>
2025-01-11 12:43:26 -0600

    remove tailwind default colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,33 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        orange: {
-          mute: "#FFA233",
-          primary: "#F7931A",
-          border: "#9E5D0F",
-        },
-        gray: {
-          50: "#eeeeee",
-          100: "#b4b4b4",
-          200: "#7b7b7b",
-          300: "#6e6e6e",
-          400: "#606060",
-          500: "#484848",
-          600: "#3a3a3a",
-          700: "#313131",
-          800: "#2a2a2a",
-          900: "#222222",
-          950: "#191919",
-          1000: "#111111",
-        },
-      },
-    },
-    fontFamily: {
-      sans: ["var(--font-inter)"],
-      mono: ["var(--font-roboto-mono)"],
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		colors: {
+			orange: {
+				mute: "#FFA233",
+				primary: "#F7931A",
+				border: "#9E5D0F",
+			},
+			gray: {
+				50: "#eeeeee",
+				100: "#b4b4b4",
+				200: "#7b7b7b",
+				300: "#6e6e6e",
+				400: "#606060",
+				500: "#484848",
+				600: "#3a3a3a",
+				700: "#313131",
+				800: "#2a2a2a",
+				900: "#222222",
+				950: "#191919",
+				1000: "#111111",
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;

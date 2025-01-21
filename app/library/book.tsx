ab992ca5f8ddc40a5761033fd8cc421a68d77749
0x6A6471 <hello@jakequinter.io>
2025-01-21 13:18:05 -0600

    feat: testing bg

--- app/library/book.tsx
+++ app/library/book.tsx
@@ -39,7 +39,7 @@ export default function BookItem({ book }: Props) {
 				<button
 					className={cn(
 						isOpen ? "invisible" : "",
-						"group relative flex w-full flex-col rounded-[20px] bg-black/80 p-4 text-left focus:outline-none",
+						"group relative flex w-full flex-col rounded-[20px] bg-black p-4 text-left focus:outline-none",
 					)}
 				>
 					<Icon

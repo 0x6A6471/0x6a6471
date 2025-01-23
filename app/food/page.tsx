150f6caaed06294f520ff57a8d386ae999075ce9
0x6A6471 <hello@jakequinter.io>
2025-01-23 11:10:58 -0600

    chore: formatting

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -4,7 +4,7 @@ export default function FoodPage() {
 			className="flex flex-col items-center justify-center"
 			style={{ minHeight: "calc(100vh - 176px)" }}
 		>
-			<h1 className="text-xl font-bold">Coming soon</h1>
+			<h1 className="font-bold text-xl">Coming soon</h1>
 		</div>
 	);
 }

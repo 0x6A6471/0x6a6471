99948a7f300bc3952cdf7b1ff60bc227fa96f4a5
0x6A6471 <hello@jakequinter.io>
2025-01-23 11:09:51 -0600

    feat: coming soon for food and blog

---
+++ app/food/page.tsx
@@ -0,0 +1,10 @@
+export default function FoodPage() {
+	return (
+		<div
+			className="flex flex-col items-center justify-center"
+			style={{ minHeight: "calc(100vh - 176px)" }}
+		>
+			<h1 className="text-xl font-bold">Coming soon</h1>
+		</div>
+	);
+}

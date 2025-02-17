1f17362a5f6aa476f23ac3372a302c2f3071e3a0
0x6A6471 <hello@jakequinter.io>
2025-02-17 08:03:40 -0600

    feat: 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,9 @@
-import Link from "next/link";
+import Image from "next/image";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "~/404",
+};
 
 export default function NotFound() {
 	return (
@@ -6,13 +11,13 @@ export default function NotFound() {
 			className="flex flex-col items-center justify-center space-y-2"
 			style={{ minHeight: "calc(100vh - 176px)" }}
 		>
-			<h1 className="font-semibold text-gray-700">404</h1>
-			<Link
-				href="https://fuckthebears.org"
-				className="relative text-orange-primary"
-			>
-				Don&apos;t click me
-			</Link>
+			<h1 className="font-semibold text-gray-700 mb-8">Not found</h1>
+			<Image
+				src="/images/not-found.png"
+				alt="Buy Bitcoin Sign"
+				height={288}
+				width={512}
+			/>
 		</div>
 	);
 }
